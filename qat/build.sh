f3d7083c6ab7a0e40d1285d88fb4314a806fba01
Liang Fang <liang.a.fang@intel.com>
2020-10-13 15:18:45 +0800

    add build.sh

---
+++ qat/build.sh
@@ -0,0 +1,32 @@
+CUR_DIR=`pwd`
+mkdir qat1.x
+cd qat1.x
+
+#download qat package from 01.org, latest package can be found at https://01.org/intel-quickassist-technology
+QAT_TAR_PKG=qat1.7.l.4.9.0-00008.tar.gz
+wget https://01.org/sites/default/files/downloads/$QAT_TAR_PKG
+
+tar -zxvf $QAT_TAR_PKG
+
+#compile qat library
+export ICP_ROOT=`pwd`
+./configure
+make
+sudo make install
+
+#compile md5accel module
+MD5DIR=$ICP_ROOT/quickassist/lookaside/access_layer/src/sample_code/functional/sym/md5accel
+
+mkdir $MD5DIR
+cp ../qat_hash.c $MD5DIR/
+cp ../qat_hash.h $MD5DIR/
+cp ../qat_utils.h $MD5DIR/
+cp ../Makefile $MD5DIR/
+
+cd $MD5DIR
+make
+echo $MD5DIR
+ls -l $MD5DIR
+cd $CUR_DIR
+cp $MD5DIR/libqat_hash.so .
+
